40db7a305ce3149e321ae5caaf5b57d118f7b56c
Alexander Briones <alexanderbvart@gmail.com>
2021-08-20 13:40:19 -0500

    PARAMETERS named in the POST data sending service

--- services/post.js
+++ services/post.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
-const post = async (src, data) => {
-    const response = await axios.post(src, data)
+const post = async ({src, data}) => {
+    const API_URL = process.env.NEXT_PUBLIC_API_PORT
+    const response = await axios.post(`${API_URL}/${src}`, data)    
     return response
 }
 export default post;
